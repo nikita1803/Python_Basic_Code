9897112982bf3b69451640da02bb286524ca26b2
Nikita Rai <rainikita595@gmail.com>
2021-08-09 13:14:19 +0530

    [Nikita Rai] Add: Calculating the set differences

---
+++ set_codes/set_Difference.py
@@ -0,0 +1,12 @@
+'''
+
+@Author: Nikita Rai
+@Date: 2021-08-08 8:30:00
+@Last Modified by: Nikita Rai
+@Last Modified time: 2021-08-08 8:30:00
+@Title : Write a Python program to create set difference.
+'''
+Set_1 = {10, 11, 12, 13, 14}
+Set_2 = {12, 13, 18, 14, 16}
+print (Set_1.difference(Set_2))
+print (Set_2.difference(Set_1))

41cf36e56ae6a68fc3ab356a123d4bba83453139
Nikita Rai <rainikita595@gmail.com>
2021-08-09 22:16:27 +0530

    [Nikita Rai] Add: Calculating the occurance of the string

---
+++ String_code/occurence_Of_Substring.py
@@ -0,0 +1,12 @@
+'''
+@Author: Nikita Rai
+@Date: 2021-08-09 4:30:30
+@Last Modified by: Nikita Rai
+@Last Modified time: 2021-08-09 4:30:30
+@Title : Write a Python program to count occurrences of a substring in a string.
+
+'''
+string = 'This is a custom fellowship for uni.'
+print()
+print(string.count("for"))
+print()

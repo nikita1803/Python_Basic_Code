d1194fcff5154dff0786878be9dfd2e6e72d5984
Nikita Rai <rainikita595@gmail.com>
2021-08-09 22:10:33 +0530

    [Nikita Rai] Add: Converted the uppercase letter into the lower case

---
+++ String_code/lowercase_String.py
@@ -0,0 +1,11 @@
+'''
+@Author: Nikita Rai
+@Date: 2021-08-09 4:20:30
+@Last Modified by: Nikita Rai
+@Last Modified time: 2021-08-09 4:20:30
+@Title : Write a Python program to lowercase first n characters in a string.
+
+'''
+string = 'NIKITA RAI'
+print("Original String:" , string)
+print("after converting first 4 elements in lowercase",string[:4].lower() + string[4:])

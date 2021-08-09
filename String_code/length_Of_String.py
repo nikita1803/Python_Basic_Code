e93d1b06ec7d6d7c65b8d71d67681e3d94cd2c0f
Nikita Rai <rainikita595@gmail.com>
2021-08-09 22:13:53 +0530

    [Nikita Rai] Add: Calculating the length of the string

---
+++ String_code/length_Of_String.py
@@ -0,0 +1,14 @@
+'''
+@Author: Nikita Rai
+@Date: 2021-08-09 4:10:30
+@Last Modified by: Nikita Rai
+@Last Modified time: 2021-08-09 4:10:30
+@Title : Write a Python program to calculate the length of a string.
+
+'''
+str = input("Enter a string: ")
+count = 0
+
+for s in str:
+      count = count+1
+print("Length of the input string is:", count)

85d3e816a717bd73127f3c239bf0cae405f34171
Nikita Rai <rainikita595@gmail.com>
2021-08-09 13:39:11 +0530

    [Nikita Rai] Add: Created a tuple

---
+++ tupple_codes/create_a_tuple.py
@@ -0,0 +1,10 @@
+'''
+@Author: Nikita Rai
+@Date: 2021-08-07 6:00:00
+@Last Modified by: Nikita Rai
+@Last Modified time: 2021-08-07 6:00:00
+@Title : Write a Python program to create a tuple.
+
+'''
+tuple = ("Nikita", True, 2,3, 1.3)
+print("Tuple elements are :",tuple)

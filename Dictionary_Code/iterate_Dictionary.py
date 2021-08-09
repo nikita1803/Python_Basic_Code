22ee925192d4d7f33f74078b2eb0c88e71710719
Nikita Rai <rainikita595@gmail.com>
2021-08-09 12:48:12 +0530

    [Nikita Rai] Add: iterate over dictionaries using for loops

---
+++ Dictionary_Code/iterate_Dictionary.py
@@ -0,0 +1,10 @@
+'''
+@Author: Nikita Rai
+@Date: 2021-08-08 06:30:00
+@Last Modified by: Nikita Rai
+@Last Modified time: @Date: 2021-08-08 06:30:00
+@Title : Write a Python program to iterate over dictionaries using for loops.
+'''
+Dictionary = {'Nikita': 1, 'Rai': 2, 'MCA': 3} 
+for key, value in Dictionary.items():
+     print(key, 'corresponds to ', Dictionary[key]) 

8494ff1c1cee3aa12a330416e11d4ab756333947
Nikita Rai <rainikita595@gmail.com>
2021-08-09 13:07:49 +0530

    [Nikita Rai] Add: Calculating the intersaction of set

---
+++ set_codes/set_Intersaction.py
@@ -0,0 +1,14 @@
+'''
+@Author: Nikita Rai
+@Date: 2021-08-08 8:40:00
+@Last Modified by: Nikita Rai
+@Last Modified time: 2021-08-08 8:40:00
+@Title : Write a Python program to create an intersection of sets.
+
+'''
+
+Set_1 = {1, 2, 3, 4} 
+Set_2 = {3, 4, 5, 6} 
+  
+print("set1 intersection set2 : ", Set_1.intersection(Set_2))
+  
